15d4fee5ba38b4ab2e4a607dea5bbf876868569f
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-01 12:10:22 -0500

    find characters

--- kevin_schmitt/compareArrays.py
+++ kevin_schmitt/compareArrays.py
@@ -7,3 +7,15 @@ if list1 == elems_in_both_lists:
     print 'These are the same list'
 else:
     print "THESE AIN'T THE SAME"
+# 
+# 
+
+list1 = [1,2,5,6,2]
+list2 = [1,2,5,7,11]
+def compareArrays(list1,list2):
+    if(list1 == list2):
+        print 'list are same'
+    else:
+        print 'they not same'
+
+compareArrays(list1,list2)
