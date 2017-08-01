5173b28afcc425633adf5e974857ca9929d72f25
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-01 17:39:09 -0500

    stars complete

--- kevin_schmitt/stars.py
+++ kevin_schmitt/stars.py
@@ -1,6 +1,19 @@
-x=[4,6,1,3,5,7,25]
+# x=[4,6,1,3,5,7,25]
+
+# def stars(arr):
+#     for i in range(0,len(arr)):
+#         print '*' * arr[i]
+# stars(x)
+
+
+x=[4,'tom',1,'michael',5,7,'jimmy smith']
 
 def stars(arr):
-    for i in range(0,len(arr)):
-        print '*' * arr[i]
+    for i in arr:
+        if isinstance(i,int):
+            print '*' * i
+        else:
+            length = len(i)
+            letter=i[0].lower()
+            print letter * length
 stars(x)
