7d5642d85b76fbc473c833dd4a5ce74bdd3e0c0d
KEVIN SCHMITT <kschmit1@aisd.net>
2017-07-31 19:28:16 -0500

    filter by type

---
+++ filtertype.py
@@ -0,0 +1,35 @@
+sI = 45
+mI = 100
+bI = 455
+eI = 0
+spI = -23
+sS = "Rubber baby buggy bumpers"
+mS = "Experience is simply the name we give our mistakes"
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+eS = ""
+aL = [1,7,4,21]
+mL = [3,5,7,34,3,2,113,65,8,89]
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+eL = []
+spL = ['name','address','phone number','social security number']
+
+i = bI
+if i >= 100:
+    print "thats a big number"
+elif i < 100:
+    print 'thats a small number'
+else:
+    print 'number aint got no value'
+
+
+string = bS
+if len(string)>=50:
+    print 'long sentence'
+elif len(string)<50:
+    print 'short sentence'
+
+list = lL
+if len(list)>=10:
+    print 'big list!'
+elif len(list)<10:
+    print 'short list!'
