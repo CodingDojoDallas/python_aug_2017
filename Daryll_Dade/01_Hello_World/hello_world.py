bef2664e68157183da7a6522bf038dd047731dc7
Daryll Dade <Darylldade@yahoo.com>
2017-08-01 14:57:25 -0500

    Adding First Python Assignments

--- Daryll_Dade/01_Hello_World/hello_world.py
+++ Daryll_Dade/01_Hello_World/hello_world.py
@@ -34,3 +34,54 @@ print x.upper()
 ninjas = ['Rozen', 'KB', 'Oliver']
 my_list = ['4', ['list', 'in', 'a', 'list'], 987]
 empty_list = []
+
+drawer = ['documents', 'envelopes', 'pens']
+#access the drawer with index of 0 and print value
+print drawer[0]  #prints documents
+#access the drawer with index of 1 and print value
+print drawer[1] #prints envelopes
+#access the drawer with index of 2 and print value
+print drawer[2] #prints pens
+
+x = [99,4,2,5,-3];
+print x[:]
+#the output would be [99,4,2,5,-3]
+print x[1:]
+#the output would be [4,2,5,-3];
+print x[:4]
+#the output would be [99,4,2,5]
+print x[2:4]
+#the output would be [2,5];
+
+my_list = [1, 'Zen', 'hi']
+print len(my_list)
+# output
+3
+
+my_list = [1,5,2,8,4]
+my_list.append(7)
+print my_list
+# output:
+# [1,5,2,8,4,7]
+
+# if statement:
+if <condition>:
+  # do something
+# if-else statement:
+elif <condition>:
+  # do something
+else:
+  # do this instead
+
+  age = 15
+if age >= 18:
+  print 'Legal age'
+else:
+  print 'You are so young!'
+
+  if age >= 18:
+  print 'Legal age'
+elif age == 17:
+  print 'You are seventeen.'
+else:
+  print 'You are so young!'
--- Daryll_Dade/02_Strings_and_Lists/Phradest_Dade_Strings_and_Lists.py
+++ Daryll_Dade/02_Strings_and_Lists/Phradest_Dade_Strings_and_Lists.py
@@ -1,3 +1,5 @@
+
+
 str = "If monkeys like bananas, then I must be a monkey!"
 print str.replace("monkeys", "Alligator", 1)
 
