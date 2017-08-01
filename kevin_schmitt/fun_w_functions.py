196c3039c29281e70fce9f5d778e2295b7fdda0f
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-01 15:37:20 -0500

    fun with functions complete

---
+++ kevin_schmitt/fun_w_functions.py
@@ -0,0 +1,7 @@
+
+for i in range(1,1001):
+    num = i
+    if (i % 2 == 1):
+        print "Number is " + str(num) + '.' + " This an odd number!"
+    else:
+        print 'Number is ' + str(num) + '.' + 'This is an even number!!'
