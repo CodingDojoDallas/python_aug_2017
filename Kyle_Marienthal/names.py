9afd7eeff0059195a3e1fcffe3a521106a13942c
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-02 14:46:22 -0500

    coin tosses:COMPLETE bike:INcomplete

--- Kyle_Marienthal/names.py
+++ Kyle_Marienthal/names.py
@@ -17,9 +17,20 @@ users = {
      {'first_name' : 'Martin', 'last_name' : 'Puryear'}
   ]
  }
-def players(arr):
-    for student in students:
-        print student['first_name'], ' ', student['last_name']
+# def players(arr):
+#     for student in students:
+#         print student['first_name'], ' ', student['last_name']
 
 
-players(students)
+# players(students)
+
+
+def students_and_teachers(obj):
+    for key in obj:
+        print key
+    for value in obj.iteritems():
+        print '{}'.format(key)
+        # print obj[key] ===== the values of the array/list
+
+
+students_and_teachers(users)
