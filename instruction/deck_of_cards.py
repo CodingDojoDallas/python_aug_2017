c5915ed731f17ceb8e11efac7f6b274168b6711e
codyw9524 <codyw9524@gmail.com>
2017-08-02 17:27:31 -0500

    added deck_of_cards.py

---
+++ instruction/deck_of_cards.py
@@ -0,0 +1,74 @@
+import random
+# Card Class
+    #represent ONE card
+    #attributes:
+        #suit
+        #value
+
+#Deck Class
+    #hold 52 card objects
+    #attributes:
+        #deck (array)
+
+#Player Class  
+    #interact with the cards and/or the deck
+    #attributes
+        #name
+        #hand (array)
+
+class Card(object):
+    def __init__(self, value, suit):
+        self.suit = suit
+        self.value = value
+
+class Deck(object):
+    def __init__(self):
+        self.reset()
+        self.shuffle()
+
+    def reset(self):
+        self.deck = []
+        suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+        values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'King', 'Queen', 'Ace']
+        for suit in suits:
+            for value in values:
+                self.deck.append(Card(value, suit))
+        return self
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
+        return self
+
+    def deal(self, players, num):
+        for i in range(num):
+            for player in players:
+                card = self.deck.pop()
+                player.hand.append(card)
+        return self
+
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+
+a = Deck()
+print 'After Shuffle:'
+for card in a.deck:
+    print "{} of {}".format(card.value, card.suit)
+
+cody = Player('Cody')
+bob = Player('Bob')
+karen = Player('Kevin')
+players = [cody, bob, karen]
+
+a.shuffle()
+
+for player in players:
+    print '*'*50
+    print "{}'s hand contains:".format(player.name)
+    for card in player.hand:
+        print "{} of {}".format(card.value, card.suit)
+
+print "Deck contains {} cards".format(len(a.deck))
