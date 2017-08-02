0f261060f204a2325c858d9dc2b1026ea37f3920
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-02 13:39:32 -0500

    bike complete

--- kevin_schmitt/dictionary_basics.py
+++ kevin_schmitt/dictionary_basics.py
@@ -1,3 +1,13 @@
 user = {'first name':'Anna','last name':'Wilson','age':'101','country':'United States','language':'python'}
 for key in user:
     print 'My ' + key +' is ' + user[key]
+
+
+# class User(object):
+#     name = "Anna"
+# anna = User()
+# print "anna's name: ", anna.name
+# User.name = "Bob"
+# print "anna's name after change:", anna.name
+# bob = User()
+# print "bob's name:", bob.name
