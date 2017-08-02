d06742c31801ca1e35d91490697da76fd9a5d200
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-02 08:50:49 -0500

    names: Part 1

---
+++ Kyle_Marienthal/names.py
@@ -0,0 +1,25 @@
+#Part 1
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+def players(arr):
+    for student in students:
+        print student['first_name'], ' ', student['last_name']
+
+
+players(students)
