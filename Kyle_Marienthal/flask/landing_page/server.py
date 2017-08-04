5dc5dd00686dd9e15add1fa5b62b16dd11ffbc32
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-04 17:47:54 -0500

    files made

---
+++ Kyle_Marienthal/flask/landing_page/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index_page():
+    return render_template(index.html)
+
+@app.route('/ninjas')
+def ninja_page():
+    return render_template(ninjas.html)
+
+@app.route('/dojos/new')
+def dojo_page():
+    return render_template(dojo.html)
+
+app.run(debug=True)
