4f7c96d1f78f2e139eb73df004d7bfadc854f032
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-04 16:54:50 -0500

    hello world: COMPLETE

--- Kyle_Marienthal/flask/hello_world/server.py
+++ Kyle_Marienthal/flask/hello_world/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def assignmentHelloWorld():
     return render_template('index.html')
 
 @app.route('/success')
 def success():
-    return render_template ('success.html')
+    return render_template('success.html')
 
 app.run(debug=True)
