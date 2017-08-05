8fadcdb31782defd61f1ecc8272b29c4972da60d
Daryll Dade <Darylldade@yahoo.com>
2017-08-05 09:43:01 -0500

    updated math_dojo and stars

--- Daryll_Dade/Python_OOP/05-Math_Dojo/05_math_dojo.py
+++ Daryll_Dade/Python_OOP/05-Math_Dojo/05_math_dojo.py
@@ -1,24 +1,23 @@
 class MathDojo(object):
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
 
-    def add(self):
-        total1= num1+num2
-        print total1
+    def __init__(self):
+        self.result = 0
+
+    def add(self, *args):
+
+        for arg in args:
+            if isinstance(arg, list or tuple):
+                for item in arg:
+                    self.result += item
+            else:
+                self.result += arg
         return self
 
-    def subtract(self):
-        total2 = num1-num2
-        print total2
+    def subtract(self, *args):
+        for arg in args:
+            if isinstance(arg, list or tuple):
+                for item in arg:
+                    self.result -= items
+            else:
+                self.result -= arg
         return self
-
-    def display_nums(self):
-        print "{}  {} health.".format(self.num1, self.num2)
-        return self
-
-ten_fifteen = MathDojo(10, 15)
-
-ten_fifteen.add().display nums
-
-    
--- Daryll_Dade/Python_OOP/05-Math_Dojo/05_math_dojo_3a.py
+++ Daryll_Dade/Python_OOP/05-Math_Dojo/05_math_dojo_3a.py
@@ -1,38 +1,21 @@
-import math
-
 class MathDojo(object):
-    def __init__(self,*nums):
+    def __init__(self):
         self.result = 0
-
-
-    def add(self, arg1, *nums):
-        # for num in nums:
-        #     if type(num) == list or type (num)== tuple:
-        #         for number in num:
-        #             self.result = num
-        #         else:
-        #             self.result = num
-        #     print "{} is the total of your input".format(self.result)
-        print ""{},{}""
-        return self.result
-
-    def subtract(self,arg1, *nums):
+    def add(self, *nums):
+        for num in nums:
+            if type(num) == list or type (num)== tuple:
+                for number in num:
+                    self.result += number
+                else:
+                    self.result += number
+                return self
+    def subtract(self, *nums):
         for num in nums:
             if type(num) == list or type(num) == tuple:
                 for number in num:
                     self.result -= number
-                else:
-                    self.result -= number
-        return self.result
+            else:
+                self.result -= number
+        return self
 
-
-x =  MathDojo(25, 20)
-print(x.add(1,2))
-y = MathDojo(100,[1,2,3,4,5])
-
-x.add(10,5)
-
-print x
-print y
-
-# print y.add()
+print MathDojo().add(5,20).result
