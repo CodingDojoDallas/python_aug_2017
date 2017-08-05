5ca1a61eab55b02c7ded574517315b43d1f315f4
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-05 16:06:20 -0500

    ninja disappear complete

--- kevin_schmitt/myEnvironments/python_stack/flask_fundamentals/disapp_ninja/disapp_ninja.py
+++ kevin_schmitt/myEnvironments/python_stack/flask_fundamentals/disapp_ninja/disapp_ninja.py
@@ -10,20 +10,29 @@ def index():
 def ninja():
     return render_template('ninja.html')
 
-@app.route('/ninja/blue')
-def blue():
-    return render_template('blue.html')
 
-@app.route('/ninja/red')
-def red():
-    return render_template('red.html')
+@app.route('/ninja/<username>')
+def show_user_profile(username):
+    print username
+    return render_template('blue.html', username=username)
 
-@app.route('/ninja/purple')
-def purple():
-    return render_template('purple.html')
 
-@app.route('/ninja/orange')
-def orange():
-    return render_template('orange.html')
+
+
+# @app.route('/ninja/blue')
+# def blue():
+#     return render_template('blue.html')
+
+# @app.route('/ninja/red')
+# def red():
+#     return render_template('red.html')
+
+# @app.route('/ninja/purple')
+# def purple():
+#     return render_template('purple.html')
+
+# @app.route('/ninja/orange')
+# def orange():
+#     return render_template('orange.html')
 
 app.run(debug=True)
