7ed2ad1439d0d14e9f046aa39fe4e0ab677ebd38
Daryll Dade <Darylldade@yahoo.com>
2017-08-05 02:38:29 -0500

    updated Dictionary_Basics and Names1,2

---
+++ Daryll_Dade/Python_Fundamentals/13-Names/Names.py
@@ -0,0 +1,8 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+for x in students:
+    print x['first_name'], x['last_name']
