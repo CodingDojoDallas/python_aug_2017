680bc0609e1ef63d20efa3b08232faa47cae42ac
Kevin Michael Camp <kevinmichaelcamp@gmail.com>
2017-08-06 13:25:38 -0500

    Flask Fundamentals - Landing Page

--- kevin_camp/flask_fundamentals/landing_page/server.py
+++ kevin_camp/flask_fundamentals/landing_page/server.py
@@ -1,9 +1,17 @@
-import flask from Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
         return render_template('index.html')
+
+@app.route('/ninjas')
+def ninjas():
+	return render_template('ninjas.html')
+
+@app.route('/dojos/new')
+def dojos():
+	return render_template('dojos/dojos.html')
 
 app.run(debug=True)
