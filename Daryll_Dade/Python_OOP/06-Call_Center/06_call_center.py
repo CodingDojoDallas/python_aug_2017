4d56ff06bb8060f9eac18b746a7ed4fafc8af626
Daryll Dade <Darylldade@yahoo.com>
2017-08-07 17:25:38 -0500

    Finished Call_Center.py

--- Daryll_Dade/Python_OOP/06-Call_Center/06_call_center.py
+++ Daryll_Dade/Python_OOP/06-Call_Center/06_call_center.py
@@ -1,31 +1,54 @@
 class Call():
-    def __init__(self,ID,caller_name,time,reason):
-        self.ID = ID
-        self.caller = caller_name
+    def __init__(self, id1, name, phone_number, time, reason):
+        self.id1 = id1
+        self.name = name
+        self.phone = phone_number
         self.time = time
         self.reason = reason
+
     def display(self):
-        print "ID: " + self.ID
-        print "Caller Name:" + self.caller
-        print "Time of call: " + self.time
-        print "Reason for call: " + self.reason
+        print "ID:" + self.id1
+        print "Name:" + self.name
+        print "Phone:" + self.phone
+        print "Time of Call" + self.time
+        print "Reason for call:" + self.reason
+#        print len(self.call)
+# Call("01", "Sean", "55555555555", "7:00 AM", "Flat Tire").display()
 
-Call("01","Bob","9:00 PM","Flat Tire.").display()
+class CallCenter():
+    def __init__(self):
+        self.queue = []
+#        print len(self.call)
 
-class callCenter():
-    def __init__(self,call):
-        self.call = call
-        self.queue = len(self.call)
-    def add(self):
-        self.call.append(self.call[len(self.call)-1]+1)
+    def add(self, call):
+        print "Adding a call to queue"
+        if len(self.queue) < 2: # 2 = maximum capacity
+            self.queue.append(call)
+            print "You are call {} in queue.".format(len(self.queue))
+        else:
+            print "All Lines are busy now."
         return self
-        #Why doesn't this work?
-    def remove(self):
-        self.call.remove(self.call[0])
+
+
+    def remove(self, call):
+        print "Removing a call from queue"
+        self.queue.remove(self.queue[0])
         return self
-        #why doesn't this work?
+
     def info(self):
-        print str(self.queue) + " calls in the queue."
+        print str(len(self.queue)) + " call/s in queue."
         return self
 
-callCenter([1,2,3]).remove().info()
+
+
+call1 = Call("01", "Sean", "55555555555", "7:00 AM", "Flat Tire")
+call2 = Call("02", "Max", "66666666666", "10:00 AM", "Radiator")
+call3 = Call("03", "Manny", "7777777777", "12:00 AM", "Gas")
+
+# CallCenter([1,2,3]).remove().info()
+# CallCenter(Call).call().display()
+# CallCenter().call
+# call1 = CallCenter(Call).call
+# print call1
+ccenter = CallCenter()
+ccenter.add(call1).add(call2).add(call3).remove(call1).info()
