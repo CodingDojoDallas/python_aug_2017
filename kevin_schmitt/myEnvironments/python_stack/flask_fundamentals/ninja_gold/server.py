2f2a41aaa2beed74db91ef103fdacb5b70f84e94
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-07 09:20:15 -0500

    ninja gold complete

--- kevin_schmitt/myEnvironments/python_stack/flask_fundamentals/ninja_gold/server.py
+++ kevin_schmitt/myEnvironments/python_stack/flask_fundamentals/ninja_gold/server.py
@@ -1,30 +1,32 @@
-from flask import Flask, render_template, redirect, request,session
+from flask import Flask, render_template, redirect, request, session
 import random
 app = Flask(__name__)
 app.secret_key = 'somethingSecret'
 
-goldAmount = 0
+
 
 @app.route('/')     #methods=['GET'] by default
-def index():    
-    return render_template('index.html', goldAmount = goldAmount)
+def index(): 
+    if 'gold' not in session: 
+        session['gold']=0
+    return render_template('index.html', gold = session['gold'])
 
 @app.route('/process_money', methods=['POST'])       #must methods=['POST'] to take in info filled out from index
 def process_money():
-    number = 0
-    button = request.form['name']
+    number = 0    
+    button = request.form['location']
     if button == 'farm':
         number = random.randint(10,21)
-        # goldAmount = goldAmount + number
+        session['gold'] += number
     elif button == 'cave':
         number = random.randint(5,11)
-        # goldAmount = goldAmount + number
+        session['gold'] += number
     elif button == 'house':
         number = random.randint(2,5)
-        # goldAmount = goldAmount + number
+        session['gold'] += number
     elif button == 'casino':
         number = random.randint(-50,51)
-        # goldAmount = goldAmount + number
+        session['gold'] += number
     
     
     return redirect('/')
