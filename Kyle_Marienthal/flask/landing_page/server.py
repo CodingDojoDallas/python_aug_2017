c382220cd5ab9ffc66811066cbee27eac76bab06
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-07 00:02:02 -0500

    tried to redirect, but couldn't get it to work

--- Kyle_Marienthal/flask/landing_page/server.py
+++ Kyle_Marienthal/flask/landing_page/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,10 +9,13 @@ def index_page():
 def ninja_page():
     return render_template("ninjas.html")
 
-@app.route('/dojos/new')
+@app.route('/dojos/new', methods=["POST"])
 def dojo_page():
     return render_template("dojos.html")
-# this is a comment
 
+@app.route('/dojos/new', methods=["POST"])
+def redirect():
+    return redirect("/")
+# not sure how to fix this one
 
 app.run(debug=True)
