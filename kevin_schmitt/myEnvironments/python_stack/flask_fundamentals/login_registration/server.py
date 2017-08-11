8e2959bc6595bee4699517545681a73483cbc983
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-11 14:48:53 -0500

    login validation 99% complete

--- kevin_schmitt/myEnvironments/python_stack/flask_fundamentals/login_registration/server.py
+++ kevin_schmitt/myEnvironments/python_stack/flask_fundamentals/login_registration/server.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
+import md5, os, binascii
 app = Flask(__name__)
 app.secret_key = 'mykeysecret'
-mysql = MySQLConnector(app,'login_users')
+mysql = MySQLConnector(app,'user_reg')
 @app.route('/')
 def index():
-    query = "SELECT * FROM users"                      # define your query
+    query = "SELECT * FROM users"                     # define your query
     users = mysql.query_db(query)  
-    # print friends                                        # run query with query_db()
+    # print friends                                   # run query with query_db()
     return render_template('index.html', users=users) # pass data to our template
-@app.route('/users', methods=['POST'])
+@app.route('/usersnew', methods=['POST'])
 def create():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    password_confirmation = request.form['password_confirmation']
+    
     is_valid = True
     if request.form['name'] == '':
         is_valid = False
@@ -18,21 +24,47 @@ def create():
     if request.form['email'] == '':
         is_valid = False
         flash("Email cannot be blank")
-    if request.form['password'] == '':
+    if len(password) < 4:
         is_valid = False
-        flash("Password cannot be blank")
+        flash("Password must be atleast than 4 letters.")
+    if password != password_confirmation:
+        is_valid = False
+        flash("Password must match Confirmation.")
 
     if is_valid == True:
-        query = 'INSERT INTO users (name, email, password, created_at, updated_at) VALUES (:name, :email, :password, NOW(), NOW())'
+        salt = binascii.b2a_hex(os.urandom(15))
+        hashed_pw = md5.new(password + salt).hexdigest()
+        query = 'INSERT INTO users (name, email, password, salt, created_at, updated_at) VALUES (:name, :email, :password, :salt, NOW(), NOW())'
         data = {
-                'name': request.form['name'],
-                'email': request.form['email'],
-                'password': request.form['password']
+                'name': name,
+                'email': email,
+                'password': password,
+                'salt': salt
                 }
         mysql.query_db(query, data)
-        return redirect('/')
+        return redirect('/success')
     else:
         return redirect('/')
+@app.route('/success')
+def success():
+        return render_template('success.html')
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    query = 'select * from users where email = :email'
+    data = { 'email': request.form['email'] }
+    user = mysql.query_db(query, data)
+
+    if len(user) == 0:
+        flash('Invalid Credentials')
+        return redirect('/')
+    else:
+        hashed_input_password = md5.new(request.form['password'] + user[0]['salt']).hexdigest()
+        if hashed_input_password == user[0]['password']:
+            return redirect('/success')
+        else:
+            flash('WRONG credentials')
+            return redirect('/')
 
 
 
