e7d063949797a39539c1038d7445fa13d0d24846
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-11 16:00:29 -0500

    login register - INcomplete

--- Kyle_Marienthal/flask_mysql/login_register/server.py
+++ Kyle_Marienthal/flask_mysql/login_register/server.py
@@ -1,18 +1,68 @@
-from flask import Flask, render_template, redirect, flash, session, 
+from flask import Flask, render_template, redirect, flash, session, request
 from mysqlconnection import MySQLConnector
+import md5, os, binascii
 
 app = Flask(__name__)
 app.secret_key = 'thesecretkey'
 
-mysql = MySQLConnector(app, 'name of your database!!!!!!!!!!!')
+mysql = MySQLConnector(app, 'login_register')
 
-@app.route('/')
+@app.route('/') #get request
 def home():
-    return rediret('/users')
+    return redirect('/users/new')
 
+@app.route('/users/new') #get request
+def users_new():
+    return render_template('new_user.html')
 
-@app.route('/users')
-def get_users():
+@app.route('/users',methods=['POST'])#POST request
+def create_user():
+    #validate the data
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+    password_confirmation = request.form['password_confirmation']
 
+    is_valid = True
+
+    if first_name == '':
+        is_valid = False
+        flash('First Name cannot be blank.')
+    if last_name == '':
+        is_valid = False
+        flash('Last Name cannot be blank.')
+    if email == '':
+        is_valid = False
+        flash('Email cannot be blank.')
+    if len(password) < 5:
+        is_valid = False
+        flash('Password cannot be blank.')
+    if password != password_confirmation:
+        is_valid = False
+        flash('Passwords didnt match')
+
+    if is_valid == True:
+        #step 1: generate the salt
+        salt = binascii.b2a_hex(os.urandom(15))
+        #step 2: hash the password
+        hashed_pw = md5.new(password + salt).hexdigest()
+        #step 3: save in the DB
+        query = "INSERT INTO users(first_name, last_name, email, password, salt, created_at, updated_at) VALUES(:first_name, :last_name, :email, :password, :salt, NOW(), NOW())"
+        data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'password': hashed_pw,
+            'salt': salt
+        }
+        mysql.query_db(query, data)
+        return redirect('/success')
+    else:
+        return redirect('/users/new')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 app.run(debug=True)
