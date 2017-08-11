50e39882d84c2d3a937100be1676002d72f3909c
unknown <shivalikhanuja.net@gmail.com>
2017-08-11 16:42:36 -0500

    submitting assign

--- shivi_khanuja/flask_mysql/loginandconfirmation/server.py
+++ shivi_khanuja/flask_mysql/loginandconfirmation/server.py
@@ -1,20 +1,60 @@
-om flask import Flask
-# import the Connector function
+from flask import Flask, render_template, redirect, flash, session, request
 from mysqlconnection import MySQLConnector
+import md5, os, binascii
 app = Flask(__name__)
-# connect and store the connection in "mysql" note that you pass the database name to the function
-mysql = MySQLConnector(app, 'mydb')
-# an example of running a query
-print mysql.query_db("SELECT * FROM users")
+app.secret_key = 'thesecretkey'
+mysql = MySQLConnector(app,'loginandreg')
+@app.route('/') #get request
+def home():
+    return redirect('/users/new')
+@app.route('/users/new') #get request
+def users_new():
+    return render_template('new_user.html')
+@app.route('/users',methods=['POST'])#POST request
+def create_user():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+    password_confirmation = request.form['password_confirmation']
 
+    is_valid = True
+    if first_name == '':
+        is_valid = False
+        flash('First_Name cannot be blank.')
+    if last_name == '':
+        is_valid = False
+        flash('Last_Name cannot be blank.')
+    if email == '':
+        is_valid = False
+        flash('Email cannot be blank.')
+    if len(password) < 8:
+        is_valid = False
+        flash('Password should be atleast eight characters')
+    if password != password_confirmation:
+        is_valid =  False
+        flash('password do not match')
 
+    if is_valid == True:
+           
+        salt = binascii.b2a_hex(os.urandom(15))
+        hashed_pw = md5.new(password + salt).hexdigest()
+        query = "INSERT INTO login  (first_name, last_name, email, password, salt, created_at, updated_at) VALUES (:first_name, :last_name, :email, :password, :salt, now(), now())"
+        data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'password': hashed_pw,
+            'salt': salt
+         }
+        mysql.query_db(query,data)
 
-
-
-
-
-
-
-
-
+ 
+        return redirect('/success')
+    else:
+        return redirect('/users/new')           
+@app.route('/success')
+def success():
+    return render_template('success.html')
+#validate the data
 app.run(debug=True)
