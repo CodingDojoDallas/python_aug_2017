8099912905ef529d4ccbefd9c2b9469c8f7ea47a
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-13 16:19:00 -0500

    the wall updated

--- Kyle_Marienthal/flask_mysql/the_wall/server.py
+++ Kyle_Marienthal/flask_mysql/the_wall/server.py
@@ -1,56 +1,62 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 import md5, os, binascii
-app = Flask(name)
+app = Flask(__name__)
 app.secret_key = 'mykeysecret'
 mysql = MySQLConnector(app,'user_reg')
+
 @app.route('/')
 def index():
-    query = "SELECT * FROM users"
+    query = "SELECT * FROM users"                    
     users = mysql.query_db(query)  
-# print friends                                  
+    # print friends                                  
     return render_template('index.html', users=users)
+
 @app.route('/usersnew', methods=['POST'])
 def create():
     name = request.form['name']
     email = request.form['email']
     password = request.form['password']
     password_confirmation = request.form['password_confirmation']
-is_valid = True
-if request.form['name'] == '':
-    is_valid = False
-    flash("Name cannot be blank")
-if request.form['email'] == '':
-    is_valid = False
-    flash("Email cannot be blank")
-if len(password) < 4:
-    is_valid = False
-    flash("Password must be atleast than 4 letters.")
-if password != password_confirmation:
-    is_valid = False
-    flash("Password must match Confirmation.")
-if is_valid == True:        
-    salt = binascii.b2a_hex(os.urandom(15))
-    hashed_pw = md5.new(password + salt).hexdigest()
-    query = 'INSERT INTO users (name, email, password, salt, created_at, updated_at) VALUES (:name, :email, :password, :salt, NOW(), NOW())'
-    data = {
-            'name': name,
-            'email': email,
-            'password': hashed_pw,
-            'salt': salt
-            }
-    users = mysql.query_db(query, data)
-    # session['user_id'] = request.form['id'] We don't enter ID in form
-    session['name'] = request.form['name']
-    session['is_logged_in'] = True
-    return redirect('/thewall')
-else:
-    return redirect('/') #for some reason this line is jamming up the server!!!!!!!!!!!!!!!!!!!
+    
+    is_valid = True
+    if request.form['name'] == '':
+        is_valid = False
+        flash("Name cannot be blank")
+    if request.form['email'] == '':
+        is_valid = False
+        flash("Email cannot be blank")
+    if len(password) < 4:
+        is_valid = False
+        flash("Password must be atleast than 4 letters.")
+    if password != password_confirmation:
+        is_valid = False
+        flash("Password must match Confirmation.")
+
+    if is_valid == True:        
+        salt = binascii.b2a_hex(os.urandom(15))
+        hashed_pw = md5.new(password + salt).hexdigest()
+        query = 'INSERT INTO users (name, email, password, salt, created_at, updated_at) VALUES (:name, :email, :password, :salt, NOW(), NOW())'
+        data = {
+                'name': name,
+                'email': email,
+                'password': hashed_pw,
+                'salt': salt
+                }
+        users = mysql.query_db(query, data)
+        # session['user_id'] = request.form['id'] We don't enter ID in form
+        session['name'] = request.form['name']
+        session['is_logged_in'] = True
+        return redirect('/thewall')
+    else:
+        return redirect('/')
+
 @app.route('/thewall')
 def thewall():
-    query = "SELECT * FROM messages"
-    messages = mysql.query_db(query)
+    query = "select users.name, messages.message_text from users join messages on users.id=messages.user_id"                    
+    messages = mysql.query_db(query)                
     return render_template('thewall.html', messages=messages)
+
 @app.route('/sessions', methods=['POST'])
 def login():
     query = 'select * from users where email = :email'
@@ -72,6 +78,7 @@ def login():
         else:
             flash('WRONG credentials')
             return redirect('/')
+
 @app.route('/message', methods=['POST'])
 def message():
     message_text = request.form['message']
@@ -86,4 +93,6 @@ def message():
     }
     mysql.query_db(query, data)
     return redirect('/thewall')
+
+
 app.run(debug=True)
