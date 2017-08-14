82ebb2ca8a88bd8c3fc0e5030a02cfd80a0ab3ba
unknown <shivalikhanuja.net@gmail.com>
2017-08-14 18:12:57 -0500

    updating

---
+++ shivi_khanuja/djangoEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shivi2\desktop\python_aug_2017\shivi_khanuja\djangoenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
