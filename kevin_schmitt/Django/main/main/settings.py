34422b1189011eb9533ee0cc0283c0d189a448b5
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-14 14:30:05 -0500

    beginning Django

--- kevin_schmitt/Django/main/main/settings.py
+++ kevin_schmitt/Django/main/main/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.first_app',
+    'apps.blogs',
+    'apps.surveys',
+    'apps.users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
