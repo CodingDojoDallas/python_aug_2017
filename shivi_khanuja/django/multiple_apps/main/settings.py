d6e91773e7f7312b3a58fd1b57593751f9028446
unknown <shivalikhanuja.net@gmail.com>
2017-08-14 20:40:05 -0500

    updating

--- shivi_khanuja/django/multiple_apps/main/settings.py
+++ shivi_khanuja/django/multiple_apps/main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.multiple_apps',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- shivi_khanuja/django/multiple_apps/main/urls.py
+++ shivi_khanuja/django/multiple_apps/main/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include('apps.blogs_app.urls')),
+    url(r'^', include('apps.surveys_app.urls')),
+    url(r'^', include('apps.users_app.urls')),
 ]
