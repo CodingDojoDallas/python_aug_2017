6ae77b498b50dbedf84f5919c48541ef147746f8
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-14 20:59:39 -0500

    multiple apps-INcomplete

--- Kyle_Marienthal/Django/multiple_apps/apps/surveys/views.py
+++ Kyle_Marienthal/Django/multiple_apps/apps/surveys/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+def index(request):
+    return render(request, 'surveys/index.html')
