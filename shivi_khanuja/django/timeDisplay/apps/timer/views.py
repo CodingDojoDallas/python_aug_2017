9528d6342c4bebaeb7562019122c4c87bf102747
unknown <shivalikhanuja.net@gmail.com>
2017-08-16 09:08:11 -0500

    updating

--- shivi_khanuja/django/timeDisplay/apps/timer/views.py
+++ shivi_khanuja/django/timeDisplay/apps/timer/views.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,'timer/index.html')
