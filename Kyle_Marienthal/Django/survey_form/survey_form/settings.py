6139a5c00a75b2bc3b373139bfc248f778167c70
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-16 13:49:40 -0500

    survey form-INcomplete

--- Kyle_Marienthal/Django/survey_form/survey_form/settings.py
+++ Kyle_Marienthal/Django/survey_form/survey_form/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.survey_form',
+    'apps.survey_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
