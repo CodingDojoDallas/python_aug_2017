288d1204c46c201edc08f396a7d2210080e82b97
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-17 17:11:40 -0500

    Books/Authors complete

--- kevin_schmitt/Django/multiple_apps_main/apps/book_authors/models.py
+++ kevin_schmitt/Django/multiple_apps_main/apps/book_authors/models.py
@@ -3,22 +3,26 @@ from django.db import models
 
   # Create your models here.
 
-class books(models.Model):
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    notes = models.TextField(1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Book(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField(1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    authors = models.ManyToManyField(Author, related_name="books")
 
-class authors(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class books_authors(models.Model):
-    book = models.ForeignKey(books, related_name='books')
-    author = models.ForeignKey(authors, related_name='authors')
+# class Book_author(models.Model):
+#     book = models.ForeignKey(Book, related_name='book')
+#     author = models.ForeignKey(Author, related_name='author')
 
     # first_name = models.CharField(max_length=255)
     # last_name = models.CharField(max_length = 255)
