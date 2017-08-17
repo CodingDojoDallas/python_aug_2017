f2ddcc49bad50c275bb7d3c1cd6b06ea94428306
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-17 16:48:48 -0500

    amadon - INcomplete

--- Kyle_Marienthal/Django/amadon/apps/buystuff/views.py
+++ Kyle_Marienthal/Django/amadon/apps/buystuff/views.py
@@ -5,5 +5,6 @@ from django.shortcuts import render, redirect, reverse
 def index(request):
     return render(request, 'buystuff/index.html') #go back here and add in the app name/index.html
 
-def thankyou(request):
-    return render(request, 'buystuff/thankyou.html')
+def buy(request):
+    
+    return render(request, '/thankyou.html')
