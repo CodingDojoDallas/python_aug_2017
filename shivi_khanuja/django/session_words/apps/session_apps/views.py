3003e5efd18868742b68c8ca981f33fa74cd5cf4
unknown <shivalikhanuja.net@gmail.com>
2017-08-16 22:10:43 -0500

    updating

--- shivi_khanuja/django/session_words/apps/session_apps/views.py
+++ shivi_khanuja/django/session_words/apps/session_apps/views.py
@@ -1,6 +1,35 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,'session_apps/index.html')
+
+def create(request):
+    
+    if request.method == 'POST':
+        print request.POST
+
+      if request.POST ['word'] == '':
+          messages.error(request,'Word cannont be blank')         
+          return redirect('/')
+
+      if 'color' not in request.POST:
+          color = 'black'
+      else:
+          color = request.POST['color']    
+      if 'font' not in request.POST:
+          font_size = 'default_font'
+      else:
+          font_size = 'large_font'
+
+      word = {
+          'value': request.POST['word'],
+          'font_size': font_size,
+          'color': color 
+      }   
+
+      request.session['word_array'].append(word)
+      print '*'*25
+      print request.session['word_array']   
+      request.session.modified = True
+
+    return redirect('/')    
