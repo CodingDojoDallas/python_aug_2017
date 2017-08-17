204985d05919b707ca719605ea7c25764208c8fa
unknown <shivalikhanuja.net@gmail.com>
2017-08-17 11:05:37 -0500

    submitting assignments

--- shivi_khanuja/django/session_words/apps/session_apps/urls.py
+++ shivi_khanuja/django/session_words/apps/session_apps/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-]
+    url (r'^$', views.index),
+    url(r'^words$', views.create),
+    url(r'^reset$', views.reset),
+  ] 
--- shivi_khanuja/django/session_words/apps/session_apps/views.py
+++ shivi_khanuja/django/session_words/apps/session_apps/views.py
@@ -1,35 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from datetime import datetime
 
 def index(request):
+    # request.session.flush()
+    if 'word_array' not in request.session:
+        request.session['word_array'] = []
+
     return render(request,'session_apps/index.html')
 
 def create(request):
     
     if request.method == 'POST':
+        print'*'*50
         print request.POST
-
-      if request.POST ['word'] == '':
-          messages.error(request,'Word cannont be blank')         
+        print '*'*50
+    if request.POST ['word'] == '' :
+          messages.error(request, 'Word cannont be blank')         
           return redirect('/')
 
-      if 'color' not in request.POST:
+    if 'color' not in request.POST:
           color = 'black'
-      else:
+    else:
           color = request.POST['color']    
-      if 'font' not in request.POST:
+    if 'font' not in request.POST:
           font_size = 'default_font'
-      else:
+    else:
           font_size = 'large_font'
 
-      word = {
-          'value': request.POST['word'],
-          'font_size': font_size,
-          'color': color 
+    word = {
+         'value' : request.POST['word'],
+         'font_size' : font_size,
+         'color' : color ,
+         'time' : datetime.now().strftime("%H:%M %p, %B %d, %Y")
       }   
 
-      request.session['word_array'].append(word)
-      print '*'*25
-      print request.session['word_array']   
-      request.session.modified = True
+    request.session['word_array'].append(word)
+      
+    request.session.modified = True
 
-    return redirect('/')    
+    return redirect('/')  
+
+def reset(request):
+    if request.method == 'POST':
+        request.session.flush()
+    return redirect('/')
+
