8bad552bd124afcb1fb354d8b990ffbb8250741d
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-17 16:48:28 -0500

    amadon - INcomplete

--- Kyle_Marienthal/Django/amadon/apps/buystuff/urls.py
+++ Kyle_Marienthal/Django/amadon/apps/buystuff/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^thankyou$', views.thankyou),
+    # url(r'^thankyou$', views.thankyou),
+    url(r'^buy$', views.buy)
 ]
