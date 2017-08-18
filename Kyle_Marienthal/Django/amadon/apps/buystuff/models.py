17e4c58fb050b225d5e14351910842d3b25ece3d
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-18 07:54:57 -0500

    first attempt at models - UNsuccessful

--- Kyle_Marienthal/Django/amadon/apps/buystuff/models.py
+++ Kyle_Marienthal/Django/amadon/apps/buystuff/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
-    age = models.IntegerField
+    age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
