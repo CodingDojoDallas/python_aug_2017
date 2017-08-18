b47a12aab71838dec47541ddd1e3fb6319557b6d
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-18 12:10:24 -0500

    dojo ninjas-INcomplete

--- Kyle_Marienthal/Django/dojo_ninjas/apps/ninja_models_app/models.py
+++ Kyle_Marienthal/Django/dojo_ninjas/apps/ninja_models_app/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    dojo = models.ForeignKey(Dojo, related_name='ninjas')
+    
