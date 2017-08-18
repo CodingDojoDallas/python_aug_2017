4bc1faaa039142c529838c8843364137900b952b
Kyle Marienthal <kylemarienthal@gmail.com>
2017-08-17 21:12:50 -0500

    first time model work

--- Kyle_Marienthal/Django/amadon/apps/buystuff/models.py
+++ Kyle_Marienthal/Django/amadon/apps/buystuff/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
