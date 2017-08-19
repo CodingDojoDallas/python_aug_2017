59462fe40d296d74dba7ba2fa5a6e4be6637c908
Daryll Dade <Darylldade@yahoo.com>
2017-08-18 22:50:05 -0500

    "Added Models to login and reg and to Books in Django Project"

--- Daryll_Dade/Django_Fundamentals/django_project/apps/login_reg/models.py
+++ Daryll_Dade/Django_Fundamentals/django_project/apps/login_reg/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
