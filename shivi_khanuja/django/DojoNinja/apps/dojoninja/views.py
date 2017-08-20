5e6fd96341ffe4b5cb99aab656bbf80bb783d805
Shivi7434 <shivalikhanuja.net@gmail.com>
2017-08-20 18:36:29 -0500

    updating

--- shivi_khanuja/django/DojoNinja/apps/dojoninja/views.py
+++ shivi_khanuja/django/DojoNinja/apps/dojoninja/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import dojoninja
+
 
 def index(request):
-    return render(request,'index.html')
+    Dojo.object.create(
+        name =request.POST['name'],
+        city =request.POST['city'],
+        state =request.POST['state']
+    )
+    return redirect('/index.html')
 
+def newList(request):
+    Dojo=Dojo.object.all()
+    context={ 'dojo' :Dojo }
+    return request(request,'dojoninja/newList.html',context)        
