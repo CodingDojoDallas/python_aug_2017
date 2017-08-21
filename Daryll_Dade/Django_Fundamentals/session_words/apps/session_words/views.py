27e91646de094e8ce08a861c52081f7444d6defc
Daryll Dade <Darylldade@yahoo.com>
2017-08-21 10:19:50 -0500

    "updated session_words "

--- Daryll_Dade/Django_Fundamentals/session_words/apps/session_words/views.py
+++ Daryll_Dade/Django_Fundamentals/session_words/apps/session_words/views.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 import datetime
-
+from django.contrib import messages
 
 def index(request):
 
-
+    print "Index Page"
     return render(request, 'session_words/index.html')
 
 
@@ -15,9 +15,9 @@ def modify(request):
     #Step 1: validation
     print "posting"
     if request.method=="POST":
-        if request.POST['name']:
-            named=request.POST['name']
-            print named
+        if request.POST['word'] == "":
+            messages.error(request, 'Word cannot be blank')
+            return redirect('/')
         # else:
         #     #flash message "Textbox cannot be empty"
         # pass
