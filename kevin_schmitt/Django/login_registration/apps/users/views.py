06816c9d71dead3c954fff48a1fbb0c6d581d678
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-20 20:07:30 -0500

    working on login_registration

--- kevin_schmitt/Django/login_registration/apps/users/views.py
+++ kevin_schmitt/Django/login_registration/apps/users/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
 # Create your views here.
 def home(request):
-    print 'index'
+    print 'THIS IS MY HOME def******************'
     return redirect('/users/new')
 
 def new(request):
-    return render(request, 'users/users.html')
+    return render(request, 'users/new.html')
 
 def create(request):
+    print request.POST
     if request.method == 'POST':
-        User.objects.validate_registration()
-
-    return redirect()
+        result = User.objects.validate_registration(request.POST)
+        print result
+        print result['status']
+        #if dict - have to use square bracket notation
+        #if class - have to use . notation
+        if result['status'] == False:
+            #create flash messages
+            for error in result['errors']:
+                messages.error(request, error)
+        return redirect('/users/new')
