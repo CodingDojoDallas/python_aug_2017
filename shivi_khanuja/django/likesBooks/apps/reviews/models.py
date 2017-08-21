ace43ec871ee6465d8bdeaaed893bd84e5da27ad
Shivi7434 <shivalikhanuja.net@gmail.com>
2017-08-21 13:40:37 -0500

    updating

--- shivi_khanuja/django/likesBooks/apps/reviews/models.py
+++ shivi_khanuja/django/likesBooks/apps/reviews/models.py
@@ -1,6 +1,17 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
-
-# Create your models here.
+  
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255) 
+    created_at =models.DateTimeField(auto_now_add=True) 
+    updated_at =models.DateTimeField(auto_now=True)
+    
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at =models.DateTimeField(auto_now_add=True) 
+    updated_at =models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User,related_name='books_liked')
+    uploader = models.ForeignKey(User, related_name='books')
--- shivi_khanuja/django/likesBooks/apps/reviews/views.py
+++ shivi_khanuja/django/likesBooks/apps/reviews/views.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render,redirect
 
-from django.shortcuts import render
-
-# Create your views here.
+def index(request):
+    return render (request,'reviews.index.html')
