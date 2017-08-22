6a551a8e5ac404508648d1d5a336214c31cb76b1
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-22 14:51:57 -0500

    login reg complete belt reviewer

---
+++ kevin_schmitt/Django/belt_review/apps/user_review/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home),
+    url(r'^new', views.new),
+    url(r'^login', views.login),
+    url(r'^register', views.register),
+    url(r'^success', views.success),
+    url(r'^logout', views.logout),
+    url(r'^add', views.add),
+]
---
+++ kevin_schmitt/Django/belt_review/apps/user_review/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
+# Create your views here.
+
+def home(request):
+    print 'this is index****************'
+    # request.session.flush()
+    return redirect('/user_review/new')
+
+def new(request):
+    print 'this is new*****************'
+    return render(request, 'user_review/new.html')
+
+
+def register(request):
+    print 'this is register***************'
+    print request.POST
+    if request.method == 'POST':
+        result = User.objects.validate_registration(request.POST)
+        print result
+        print result['status']
+        #if dict - have to use square bracket notation
+        #if class - have to use . notation
+        if result['status'] == False:
+            request.session['name'] = ''
+            #create flash messages
+            for error in result['errors']:
+                messages.error(request, error)
+            return redirect('/user_review/new')
+        else:
+            #put user_id into session
+            request.session['user_id'] = result['user'].id
+            return redirect('/user_review/success')
+
+def login(request):
+    print 'this is login**********************'
+    if request.method == 'POST':
+        result = User.objects.validate_login(request.POST)
+        print result
+        if result['status'] == False:
+            request.session['name'] = ''
+            #generate error message
+            messages.error(request, result['error'])
+            return redirect('/user_review/new')
+        else:
+            request.session['user_id'] = result['user'].id
+            request.session['name'] = result['user'].name
+            return redirect('/user_review/success')
+
+def success(request):
+    print 'this is success***************'
+    if request.session['name'] == '':
+        return redirect('/user_review')
+    else:
+        return render(request, 'user_review/wall.html')
+
+def logout(request):
+    request.session.flush()
+    return redirect('/user_review')
+
+def add(request):
+    return render(request, 'user_review/add.html')
