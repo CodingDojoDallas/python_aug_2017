dae4bf602e4598b6b7383c4163bd536cfb12a93d
KEVIN SCHMITT <kschmit1@aisd.net>
2017-08-25 16:47:47 -0500

    added login_registration1

---
+++ kevin_schmitt/Django/login_registration1/apps/users/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home),
+    url(r'^new', views.new),
+    url(r'^create', views.create),
+    url(r'^success', views.success),
+    # url(r'^login', views.login),
+    url(r'^authenticate', views.authenticate),
+    url(r'^logout', views.logout),
+]
