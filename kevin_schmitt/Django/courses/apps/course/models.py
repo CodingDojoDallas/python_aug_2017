94f7be88f849a36ad8390251c6b657ca90cfc303
KEVIN SCHMITT <kschmit1@aisd.net>
2017-09-01 03:08:14 -0500

    daily commit

--- kevin_schmitt/Django/courses/apps/course/models.py
+++ kevin_schmitt/Django/courses/apps/course/models.py
@@ -24,6 +24,7 @@ class UserManager(models.Manager):
         else:
             return { 'status': False, 'errors': errors}
 
+
 class Course(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField()
