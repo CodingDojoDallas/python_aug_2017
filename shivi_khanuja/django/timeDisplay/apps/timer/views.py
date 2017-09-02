1de895e1395464dc5ff051a7fbcd2453a1fce42d
Shivi7434 <shivalikhanuja.net@gmail.com>
2017-09-02 18:48:04 -0500

    daily commit

--- shivi_khanuja/django/timeDisplay/apps/timer/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from datetime import datetime
-
-def index(request):
-    context = {
-    'title': "The Current Time and Date",
-    'date' : datetime.now(),
-    }
-    return render(request,'timer/index.html', context)
